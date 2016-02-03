fed050816b1c51e705dab072449700095e5213da
Bocian <michbocian@gmail.com>
2016-02-03 21:16:36 +0100

    mockito tests

---
+++ 2.Mocking_tests/src/test/java/pl/edu/amu/bawsj/mockito/b/DataConcatenatorTest.java
@@ -0,0 +1,18 @@
+package pl.edu.amu.bawsj.mockito.b;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataConcatenatorTest {
+
+    @Test
+    public void shouldNameThisFunctionInACorrectWay() {
+        // given
+//        ..
+        // when
+//        ..
+        // then
+//        ..
+    }
+}
