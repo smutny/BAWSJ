efc1dc37f84abf44b09a38db6f6731f6c8c524db
Bocian <michbocian@gmail.com>
2016-02-04 21:43:23 +0100

    mockito tests

--- 2.Mocking_tests/src/main/java/pl/edu/amu/bawsj/mockito/d/UnpredictableResultProvider.java
+++ 2.Mocking_tests/src/main/java/pl/edu/amu/bawsj/mockito/d/UnpredictableResultProvider.java
@@ -7,7 +7,7 @@ public class UnpredictableResultProvider implements ResultProvider{
 
     @Override
     public double provide() {
-        return 100.0 / new Random(0).nextInt(2);
+        return (100 / new Random().nextInt(2)) * 1.2334;
     }
 
 }
