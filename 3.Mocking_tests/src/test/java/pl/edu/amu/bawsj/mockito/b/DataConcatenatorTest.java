6d0b2f60aab5d51c04cd3cbe85d9a6262489daa7
Rafał Pydyniak <rafal@pydyniak.pl>
2016-03-09 20:48:00 +0100

    Added b and c tests

--- 3.Mocking_tests/src/test/java/pl/edu/amu/bawsj/mockito/b/DataConcatenatorTest.java
+++ 3.Mocking_tests/src/test/java/pl/edu/amu/bawsj/mockito/b/DataConcatenatorTest.java
@@ -2,17 +2,18 @@ package pl.edu.amu.bawsj.mockito.b;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 public class DataConcatenatorTest {
 
     @Test
     public void shouldConcatenateSimpleStringsWithMockedDataChanger() {
-//        Data data = new Data("a", "b");
-//        DataChanger dataChanger = Mockito.mock(DataChanger.class);
-//        Mockito.do.when(dataChanger).change(data);
-//        DataConcatenator dataConcatenator = new DataConcatenator(dataChanger);
-//        Assert.assertEquals("b:c", dataConcatenator.concatenate(data));
+        Data data = Mockito.mock(Data.class);
+        Mockito.when(data.getA()).thenReturn("c");
+        Mockito.when(data.getB()).thenReturn("d");
+        DataChanger dataChanger = Mockito.mock(DataChanger.class);
+
+        DataConcatenator dataConcatenator = new DataConcatenator(dataChanger);
+        Assert.assertEquals("c:d", dataConcatenator.concatenate(data));
     }
 }
--- 3.Mocking_tests/src/test/java/pl/edu/amu/bawsj/mockito/c/StringCreatorTest.java
+++ 3.Mocking_tests/src/test/java/pl/edu/amu/bawsj/mockito/c/StringCreatorTest.java
@@ -1,17 +1,16 @@
 package pl.edu.amu.bawsj.mockito.c;
 
-import org.junit.Test;
+import org.junit.*;
+import org.mockito.Mockito;
 
 public class StringCreatorTest
 {
 
     @Test
-    public void shouldPerformSomeCalculationCorrectly()
+    public void shouldCreateStringCorrectly()
     {
-        // given
-
-        // when
-
-        // then
+        SoutPrinter soutPrinter = Mockito.mock(SoutPrinter.class);
+        StringCreator stringCreator = new StringCreator(soutPrinter);
+        Assert.assertEquals("Hi there", stringCreator.create());
     }
 }
