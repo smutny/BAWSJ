4b055a35878657547978153ed2c2f92dfaa9cf12
Rafał Pydyniak <rafal@pydyniak.pl>
2016-03-13 20:56:27 +0100

    Added C tests

---
+++ 5.Refactoring/src/test/java/pl/edu/amu/bawsj/refactoring/c/ImieninyHandlerTest.java
@@ -0,0 +1,28 @@
+package pl.edu.amu.bawsj.refactoring.c;
+
+import org.junit.*;
+import org.mockito.Mockito;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ImieninyHandlerTest {
+
+    @Test
+    public void testIfNameIsCheckedCorrectly() throws IOException {
+        BufferedReader readerMocked = Mockito.mock(BufferedReader.class);
+        Mockito.when(readerMocked.readLine())
+                .thenReturn("firstName")
+                .thenReturn("secondName")
+                .thenReturn(null);
+        ImieninyHandler imieninyHandler = new ImieninyHandler(readerMocked);
+        Assert.assertEquals(true, imieninyHandler.isNameInReader("firstName"));
+        Assert.assertEquals(false, imieninyHandler.isNameInReader("thirdName"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIfCorrectExceptionIsThrownWhenNullReaderIsGiven() throws IOException{
+        BufferedReader bufferedReader = null;
+        ImieninyHandler imieninyHandler = new ImieninyHandler(bufferedReader);
+    }
+}
