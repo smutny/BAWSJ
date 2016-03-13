42c6bb31be121504e7af07b866932a3f61c1e91c
Rafał Pydyniak <rafal@pydyniak.pl>
2016-03-13 20:56:08 +0100

    Refactored C

--- 5.Refactoring/src/main/java/pl/edu/amu/bawsj/refactoring/c/C.java
+++ 5.Refactoring/src/main/java/pl/edu/amu/bawsj/refactoring/c/C.java
@@ -12,44 +12,21 @@ public class C
 
     public static void main( String[] args ) throws IOException
     {
-        main1( "Jan" );
+        main1("Jan", "qwqw");
     }
 
     public static void main1( String... args ) throws IOException
     {
-        if( args.length == 0 )
-        {
-            System.out.println( "Nie podałeś imienia!" );
-            return;
-        }
-        InputStream inputStream = C.class.getClassLoader().getResourceAsStream( "imieniny.txt" );
+        InputStream inputStream = C.class.getClassLoader().getResourceAsStream("imieniny.txt");
         BufferedReader reader = new BufferedReader( new InputStreamReader( inputStream ) );
 
-        mainLoop:
-        for( int i = 0; i < args.length; i++ )
-        {
-            boolean found = false;
-            while( true )
-            {
-                String s = reader.readLine();
-                if( s == null )
-                {
-                    break;
-                }
-                if( s.trim().equals( args[ i ].trim() ) )
-                {
-                    found = true;
-                    break;
-                }
-            }
-            if( found )
-            {
-                System.out.println( args[ i ] + " istnieje!" );
-            }
+        ImieninyHandler imieninyHandler = new ImieninyHandler(reader);
+
+        for (String arg : args) {
+            if (imieninyHandler.isNameInReader(arg))
+                System.out.println( arg + " istnieje!" );
             else
-            {
-                System.out.println( args[ i ] + " nie istnieje!" );
-            }
+                System.out.println( arg + " nie istnieje!" );
         }
     }
 }
---
+++ 5.Refactoring/src/main/java/pl/edu/amu/bawsj/refactoring/c/ImieninyHandler.java
@@ -0,0 +1,35 @@
+package pl.edu.amu.bawsj.refactoring.c;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rafal on 3/13/16.
+ */
+public class ImieninyHandler {
+    private List<String> names;
+
+    public ImieninyHandler(BufferedReader reader) throws IOException{
+        names = new ArrayList<String>();
+
+        if (reader == null)
+            throw new IllegalArgumentException();
+
+        prepareNames(reader);
+    }
+
+    private void prepareNames(BufferedReader reader) throws IOException {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            names.add(line);
+        }
+    }
+
+    public boolean isNameInReader(String name) {
+        return names.contains(name);
+    }
+}
